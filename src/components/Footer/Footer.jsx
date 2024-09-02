3f958919fea961f03674702910de6cf7218e05e7
ezzbakry <ezzeldenamer@gmail.com>
2024-09-02 20:49:06 +0300

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import { Component } from "react";
+ 
+export default class Footer extends Component{
+    constructor(){
+        super()
+    }
+    render(){
+        return <div>hello Footer</div>
+    }
+}
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,39 @@
+import { Component } from "react";
+ 
+export default class Navbar extends Component{
+    constructor(){
+        
+        
+        
+        super()
+        this.state={
+            name:"ezz"
+        }
+        console.log("con called")
+
+    }
+    componentDidMount(){
+        console.log("cdm called")
+    }
+    componentDidUpdate(){
+        
+        console.log("update")
+    }
+
+    change=()=>{
+        this.setState({name:(this.state.name=="ezz")?"ahmed":"ezz"})
+
+    }
+    render(){
+        console.log("render called")
+        return(
+        <>
+        <div>hello {this.state.name}</div>
+        <button onClick={this.change}>change</button>
+        
+        </>
+        );
+         
+        
+    }
+}
