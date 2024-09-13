bb4d92ad7ac13a801fad9ccbec56e439881f4dc7
ezzbakry <ezzeldenamer@gmail.com>
2024-09-13 20:53:48 +0300

    namereduce correcting

--- src/Store/store.js
+++ src/Store/store.js
@@ -1,13 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import lanreduce from "./slices/language"
 import counterreduce from "./slices/counter"
-import namerduce from'./slices/name'
+import namereduce from'./slices/name'
 
 const store=configureStore({
     reducer:{
         language : lanreduce,
         counter:counterreduce,
-        name:namerduce
+        name:namereduce
     }
 })
 
